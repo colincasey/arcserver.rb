88c9a0443878259ac6e1f319f0ad2775ade7dc05
Luca Simone <info@lucasimone.info>
2013-11-13 22:08:22 +0100

    Trying to fix TravisCI build

--- lib/arcserver.rb
+++ lib/arcserver.rb
@@ -1,34 +1,37 @@
 # encoding: utf-8
 
-require 'ostruct'
-require 'httparty'
-require 'forwardable'
-require 'rufus-scheduler'
-require 'active_support/all'
-# require 'active_record'
-require 'active_support/core_ext/hash/indifferent_access'
+module ArcServer
 
-require 'arcserver/version'
-require 'arcserver/url_helper'
-require 'arcserver/queryable'
-require 'arcserver/identifiable'
-require 'arcserver/map_server'
-require 'arcserver/feature_server'
-require 'arcserver/gp_server'
+  require 'ostruct'
+  require 'httparty'
+  require 'forwardable'
+  require 'rufus-scheduler'
+  require 'active_support/all'
+  require 'active_support/core_ext/hash/indifferent_access'
 
-require 'arcserver/geometry/geometry'
+  require 'arcserver/version'
+  require 'arcserver/url_helper'
+  require 'arcserver/queryable'
+  require 'arcserver/identifiable'
+  require 'arcserver/map_server'
+  require 'arcserver/feature_server'
+  require 'arcserver/gp_server'
 
-require 'arcserver/graphics/feature_set'
-require 'arcserver/graphics/feature'
+  require 'arcserver/geometry/geometry'
 
-require 'arcserver/rest/map_server'
-require 'arcserver/rest/feature_server'
-require 'arcserver/rest/query'
-require 'arcserver/rest/identify'
-require 'arcserver/rest/gp_server'
+  require 'arcserver/graphics/feature_set'
+  require 'arcserver/graphics/feature'
 
-# relative_load_paths = %w[ arcserver ]
-# ActiveSupport::Dependencies.autoload_paths += relative_load_paths
+  require 'arcserver/rest/map_server'
+  require 'arcserver/rest/feature_server'
+  require 'arcserver/rest/query'
+  require 'arcserver/rest/identify'
+  require 'arcserver/rest/gp_server'
+
+  # relative_load_paths = %w[ arcserver ]
+  # ActiveSupport::Dependencies.autoload_paths += relative_load_paths
+
+end
 
 # monkeypath OpenStruct
 class OpenStruct
--- spec/shared_context.rb
+++ spec/shared_context.rb
@@ -4,13 +4,13 @@ shared_context "shared stuff" do
 
   def query_for_random_feature
     feature_server = ArcServer::FeatureServer.new("http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/SanFrancisco/311Incidents/FeatureServer")
-    feature_set = feature_server.query('0', { where: "status=1", outFields: "*", returnGeometry: true })
+    feature_set = feature_server.query('0', { where: "status=1", outFields: "*" })
     feature_set.features[ Random.rand(1000) ]
   end
 
   def query_for_id(id)
     feature_server = ArcServer::FeatureServer.new("http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/SanFrancisco/311Incidents/FeatureServer")
-    feature_set = feature_server.query('0', { objectIds: id, outFields: "*", returnGeometry: true })
+    feature_set = feature_server.query('0', { objectIds: id, outFields: "*" })
     feature_set.features[0]
   end
 
