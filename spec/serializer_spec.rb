7df2b67152b0b2b83c3963a7e357f273bcfcfc29
Luca Simone <info@lucasimone.info>
2014-03-16 18:00:03 +0100

    Adding some specs for serialisation
    More specs needed…

---
+++ spec/serializer_spec.rb
@@ -0,0 +1,50 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe 'Serialize' do
+
+  it 'Serialize a feature to json' do
+
+    attrs = {
+      status:    "1",
+      req_id:    "12345",
+      req_type:  "Graffiti Complaint – Private Property",
+      req_date:  "30.09.2013",
+      req_time:  "14:00",
+      address:   "via dei matti 1",
+      district:  "Lugano"
+    }
+
+    geometry = { x: 1, y: 2 }
+    f = ArcServer::Graphics::Feature.new({ geometry: geometry, attributes: attrs })
+
+    json = f.to_json
+    json.should include "\"geometry\":{\"x\":1,\"y\":2}"
+    json.should include "\"attributes\":{\"status\":\"1\",\"req_id\":\"12345\",\"req_type\":\"Graffiti Complaint \\u2013 Private Property\",\"req_date\":\"30.09.2013\",\"req_time\":\"14:00\",\"address\":\"via dei matti 1\",\"district\":\"Lugano\"}"
+
+  end
+
+  it 'serialize a point' do
+
+    geometry = ArcServer::Geometry::Point.new({ x: 997986.50, y: 5783631.06, spatialReference: { wkid: 102100 }})
+
+    f = ArcServer::Graphics::Feature.new({ geometry: geometry, attributes: {} })
+
+    json = f.to_json
+    json.should include "\"geometry\":{\"x\":997986.50,\"y\":5783631.06}"
+
+  end
+
+  it 'serialize a polygon' do
+
+    geometry = ArcServer::Geometry::Geometry.create('{"rings" : [[[-97.06138,32.837],[-97.06133,32.836],[-97.06124,32.834],[-97.06127,32.832],[-97.06138,32.837]],[[-97.06326,32.759],[-97.06298,32.755],[-97.06153,32.749],[-97.06326,32.759]]],"spatialReference" : {"wkid" : 4326}}')
+    f = ArcServer::Graphics::Feature.new({ geometry: geometry, attributes: {} })
+
+    json = f.to_json
+    json.should include "\"rings\":{"
+    json.should include "[[[-97.06138,32.837],[-97.06133,32.836]"
+
+  end
+
+end
